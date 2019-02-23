27a6834de6344f8a85ca1501637f7fde536e44fc
Ross Hathaway <hathaway.ross@gmail.com>
2019-02-23 00:57:07 -0500

    have basic reviews section
    will not render individual reviews

--- client/src/reviews.js
+++ client/src/reviews.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import SingleReview from './singleReview'
 
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
+  render() {
+    if (props.reviews) {
+      return (
+        <div>
+          <h3>Showing 1-8 of {props.total} reviews</h3>
+          <form>
+            <select>
+              <option>Top Reviews</option>
+              <option>Most recent</option>
+            </select>
+          </form>
+          {props.Reviews.map((rev, i) => (<SingleReview key={i} review={rev}/>))}
+        </div>)
+    } else {
+      return <div></div>
+    }
+  }
+}
 
 export default Reviews
